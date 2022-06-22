848261e99cd33601ea252387dc82508f59754b7a
Stanislav Gavrilchik <dev_gs@bankiros.ru>
2022-06-22 23:19:22 +0300

    feat(forms): add form PHP-1 - add form

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP-UP</title>
+    <meta charset="utf-8"/>
+</head>
+<body>
+<h2>Загрузка файла</h2>
+<form action="formHandler.php" method="post" enctype="multipart/form-data">
+    Выберите файл: <input type="file" name="filename" size="10"/><br/><br/>
+    <input type="submit" value="Загрузить"/>
+</form>
+</body>
+</html>
